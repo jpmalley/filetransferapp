9b72f1f46e8fa19eee57f1cf0c4ffff8b09fb97e
John Malley <jpmalley@me.com>
2023-03-13 21:36:19 -0600

    reconfigured static location

--- filetransfer/filetransfer/settings.py
+++ filetransfer/filetransfer/settings.py
@@ -128,13 +128,12 @@ STATICFILES_FINDERS = [
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
